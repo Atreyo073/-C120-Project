93460cd8358e015af9a034765906ac2ee4996373
Atreyo073 <107910400+Atreyo073@users.noreply.github.com>
2023-02-02 21:02:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier('Google Lens',modelLoaded);
+  }
+  function modelLoaded()
+  {
+    console.log('Model Loaded!');
+  }
